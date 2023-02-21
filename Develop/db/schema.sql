03958f46221a4442b46bbe78bc82bbcbe5127867
teresarod11 <rodriguez49372@gmail.com>
2023-02-21 17:19:25 -0500

    added tables in schema.sql and added columns in catergory model

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,37 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+CREATE TABLE Category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    catergory_id INT,
+    FOREIGN KEY (catergory_id)
+    REFERENCES Category(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE Tag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(30)
+);
+
+CREATE TABLE ProductTag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id)
+    REFERENCES Product(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (tag_id)
+    REFERENCES Tag(id)
+    ON DELETE SET NULL
+);
